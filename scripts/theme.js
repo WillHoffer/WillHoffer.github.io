17bc9cacbd00db9ee8791ed56f06d75e6227b97d
Will Hoffer <whoffer3@gmail.com>
2020-11-04 23:54:51 -0800

    Light and Dark Theme Refinements

--- scripts/theme.js
+++ scripts/theme.js
@@ -3,6 +3,13 @@ var themeIndex=0;
 var themes=["light","dark"];
 var lightBlue = "#4DC8FF";
 var royalBlue = "#0300AB";
+var darkGray = "#171717";
+var lightGray = "#B5B5B5";
+var lighterGray = "#E0E0E0";
+var periwinkle = "#BB87FF";
+var darkerPeriwinkle = "#9977CC";
+var darkLink = "#6866CC";
+var lightLink = "#0099E0";
 
 function toggleTheme(){
   themeIndex++;
@@ -17,20 +24,23 @@ function setTheme(newTheme){
   
   switch(newTheme){
     case "dark":
-      setThemeColors("black",lightBlue);
+      setThemeColors(darkGray,lightBlue,lightGray,darkLink);
       break;
     case "light":
-      setThemeColors("white",royalBlue);
+      setThemeColors(lighterGray,royalBlue,"black",lightLink);
       break;
     default:
       // Same as light theme
-      setThemeColors("white",royalBlue);
+      setThemeColors(lighterGray,royalBlue,"black",lightLink);
   }
 }
 
-function setThemeColors(colorBG,colorHead){
+function setThemeColors(bgColor,headingColor,textColor,linkColor){
   let root = document.documentElement;
   
-  document.body.style.backgroundColor=colorBG;
-  root.style.setProperty('--section-headings-color',colorHead);
+  //document.body.style.backgroundColor=bgColor;
+  root.style.setProperty('--bg-color',bgColor);
+  root.style.setProperty('--section-headings-color',headingColor);
+  root.style.setProperty('--body-text-color',textColor);
+  root.style.setProperty('--body-link-color',linkColor);
 }
