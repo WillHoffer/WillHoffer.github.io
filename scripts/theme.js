d7d8ea4053001d077e57f6d174946422f1ae04de
Will Hoffer <whoffer3@gmail.com>
2020-11-11 13:11:32 -0800

    Cleanup & purple theme

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,35 +1,62 @@
 
 // Static Variables
-var themes=["light","dark"];
-var lightBlue = "#4DC8FF";
-var royalBlue = "#0300AB";
+var themeNames=["light","dark","purple"];
+var lightBlue = "#4DC8FF"; // Capri
+var royalBlue = "#0300AB"; // Trypan Blue
 var darkGray = "#171717";
 var lightGray = "#B5B5B5";
-var lighterGray = "#E0E0E0";
+var lighterGray = "#E0E0E0"; 
 var periwinkle = "#BB87FF";
 var darkerPeriwinkle = "#9977CC";
 var darkLink = "#6866CC";
 var lightLink = "#0099E0";
 
+var russianViolet = "#34113F";
+var ecru = "#D8C99B";
+var darkOliveGreen = "#657153";
+
+var lightColors ={
+  background  : lighterGray,
+  primary     : royalBlue,
+  text        : "black",
+  links       : lightLink
+};
+
+var darkColors ={
+  background  : darkGray,
+  primary     : lightBlue,
+  text        : lightGray,
+  links       : darkLink
+};
+
+var purpleColors ={
+  background  : russianViolet,
+  primary     : darkerPeriwinkle,
+  text        : "white",
+  links       : lightLink
+};
+
 function toggleTheme(){
   var themeIndex = localStorage.getItem("whThemeIndex");
   themeIndex++;
-  if(themeIndex==themes.length)
+  if(themeIndex==themeNames.length)
     themeIndex=0;
 
   setTheme(themeIndex);
 }
 
 function setTheme(newIndex){
-  let root = document.documentElement;
   localStorage.setItem("whThemeIndex",newIndex);
   
-  switch(themes[newIndex]){
+  switch(themeNames[newIndex]){
     case "dark":
       setThemeColors(darkGray,lightBlue,lightGray,darkLink);
       break;
     case "light":
       setThemeColors(lighterGray,royalBlue,"black",lightLink);
+      break;
+    case "purple":
+      setThemeColors(russianViolet,darkerPeriwinkle,"white",lightBlue);
       break;
     default:
       // Same as light theme
@@ -41,7 +68,7 @@ function setThemeColors(bgColor,headingColor,textColor,linkColor){
   let root = document.documentElement;
   
   root.style.setProperty('--bg-color',bgColor);
-  root.style.setProperty('--section-headings-color',headingColor);
+  root.style.setProperty('--primary-color',headingColor);
   root.style.setProperty('--body-text-color',textColor);
   root.style.setProperty('--body-link-color',linkColor);
 }
