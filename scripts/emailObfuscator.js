88208d4de7f7cad52aaf02ef8de179284e6504b7
Will Hoffer <61263046+WillHoffer@users.noreply.github.com>
2021-12-21 20:22:40 -0500

    Page update and email obf

---
+++ scripts/emailObfuscator.js
@@ -0,0 +1,18 @@
+/*
+ *  Obfuscated email javascript generator. Uses the site: 
+ *
+ *     http://rot13.florianbersier.com/
+ *
+ */
+
+function setEmail() {
+    document.getElementById("hoffer-ucr-email").innerHTML="<n uers=\"znvygb:jubss003@hpe.rqh?fhowrpg=[Zngu 7N] Dhrfgvba nobhg ...\" gnetrg=\"_oynax\">Rznvy Zr</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+}
+
+window.onload = function () {
+    
+    document.getElementById("hoffer-ucr-email").innerHTML="<n uers=\"znvygb:jubss003@hpe.rqh?fhowrpg=[Zngu 7N] Dhrfgvba nobhg ...\" gnetrg=\"_oynax\">Rznvy Zr</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+    //document.body.appendChild(eo);
+
+};
+
