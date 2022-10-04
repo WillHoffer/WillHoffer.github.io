762ad7a9bb2448dc44dd3be39b1baf015826dd16
Will Hoffer <61263046+WillHoffer@users.noreply.github.com>
2022-10-04 13:27:58 -0700

    email obfuscator on homepage

--- scripts/emailObfuscator.js
+++ scripts/emailObfuscator.js
@@ -9,10 +9,19 @@ function setEmail() {
     document.getElementById("hoffer-ucr-email").innerHTML="<n uers=\"znvygb:jubss003@hpe.rqh?fhowrpg=[Zngu 7N] Dhrfgvba nobhg ...\" gnetrg=\"_oynax\">Rznvy Zr</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
 }
 
-window.onload = function () {
+function setEmail2() {
+    document.getElementById("email-goes-here").innerHTML="<n uers=\"znvygb:zngu@jvyyubssre.pbz?fhowrpg=[Jrofvgr] Lbhe fhowrpg urer\" gnetrg=\"_oynax\">Rznvy Jvyy</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+    //document.body.appendChild(eo);
+
+    document.getElementById("show-email-button").innerHTML = "";
+}
+
+document.onload += function () {
     
     document.getElementById("hoffer-ucr-email").innerHTML="<n uers=\"znvygb:jubss003@hpe.rqh?fhowrpg=[Zngu 7N] Dhrfgvba nobhg ...\" gnetrg=\"_oynax\">Rznvy Zr</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
     //document.body.appendChild(eo);
 
-};
+    document.getElementById("email-goes-here").innerHTML="<n uers=\"znvygb:zngu@jvyyubssre.pbz?fhowrpg=[Jrofvgr] Lbhe fhowrpg urer\" gnetrg=\"_oynax\">Rznvy Jvyy</n>".replace(/[a-zA-Z]/g,function(c){return String.fromCharCode((c<="Z"?90:122)>=(c=c.charCodeAt(0)+13)?c:c-26);});
+    document.body.appendChild(eo);
 
+};
